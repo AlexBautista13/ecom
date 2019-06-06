33cf20e1e78f269a8fc990e00e0969e4e8673666
alexb24 <lakers76767@gmail.com>
2019-06-06 16:20:39 -0700

    setup reduxform

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, Switch, Route } from 'react-router-dom';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 import './style/main.scss';
 
@@ -21,11 +22,15 @@ function main() {
         <Layout>
           <Switch>
             <Route path='/' exact component={Signin}/>
+            <Route path='/signin' exact component={Signin}/>
+            <Route path='/signup' exact component={Signup}/>
           </Switch>
         </Layout>
       </Router>
     </Provider>
     , document.querySelector('.app-wrapper'));
+    
+  
 }
 
 document.addEventListener('DOMContentLoaded', main);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form'; 
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  form
 });
 
 export default rootReducer;
-
-// import { combineReducers } from 'redux';
-// import { reducer as form } from 'redux-form'; 
-
-// import headerNavbar from './headernavbarReducer';
-// import user from './userReducer';
-// import shop from './shopReducer';
-
-// const rootReducer = combineReducers({
-//   form,
-//   headerNavbar,
-//   user,
-//   shop
-// });
-
-// export default rootReducer;
