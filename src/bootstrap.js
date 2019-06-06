b736bdafed635a1a81b65825d09b40242e86993e
alexb24 <lakers76767@gmail.com>
2019-06-06 15:43:15 -0700

    setup project

---
+++ src/bootstrap.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+import './style/main.scss';
+
+import Layout from './components/layout';
+
+
+function main() {
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+        <Layout>
+
+        </Layout>
+      </BrowserRouter>
+    </Provider>
+    , document.querySelector('.app-wrapper'));
+}
+
+document.addEventListener('DOMContentLoaded', main);
