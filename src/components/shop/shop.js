3d234fec0d09ac42e4ad6b76edbf2b130b63f017
alexb24 <lakers76767@gmail.com>
2019-06-10 00:16:57 -0700

    built shop search bar form and form field

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import ShopSearchBar from './shopSearchBar';
+
 class Shop extends Component {
+
     componentDidMount() {
         const headerLinks = [
             {
@@ -22,11 +26,16 @@ class Shop extends Component {
         return true
     }
 
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
 
         return (
             <div className='shop'>
-                {/* shop search bar */}
+
+                <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
                 <div className='shop__products'>
                     {
                         this.props.filteredProducts.map(product => {
@@ -48,7 +57,6 @@ class Shop extends Component {
         )
     }
 }
-
 function mapStateToProps(state) {
     const { categories, filteredProducts } = state.shop;
     return {
@@ -56,6 +64,5 @@ function mapStateToProps(state) {
         filteredProducts
     } 
 }
-
 Shop = connect(mapStateToProps, actions)(Shop);
 export default Shop;
---
+++ src/components/shop/shopSearchBar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import { reduxForm, Field } from 'redux-form';
+
+function FormSearchBar(props) {
+    return (
+        <input className={`${props.className} form-search-bar`} {...props.input} type='text' placeholder={`${props.placeholder}`} />
+    )
+}
+
+{/* <i class="fas fa-search"></i> */}
+
+class ShopSearchBar extends Component {
+    render() {
+        const { className, handleSubmit } = this.props;
+        return (
+            <form onSubmit={handleSubmit} className={`${className} shop-search-bar`}>
+                <Field name='shop-search-bar' className='shop-search-bar__form-search-bar' placeholder='search' component={FormSearchBar} />
+            </form>
+        )
+    }    
+}
+
+ShopSearchBar = reduxForm({
+    form: 'ShopSearchBar'
+})(ShopSearchBar);
+
+export default ShopSearchBar; 
