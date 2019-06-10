9599178f0d16e7e55ef97ffd2541c22171903c2d
alexb24 <lakers76767@gmail.com>
2019-06-10 13:46:16 -0700

    structured cart content products component and stkyled title

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 function CartButton({className, icon}) {
     return (
         <div className={`${className} cart-button`}>
@@ -8,21 +7,30 @@ function CartButton({className, icon}) {
     )
 }
 
-function CartContent({className}) {
+
+function CartContent({className, products}) {
+    let count = products.length;
     return (
         <div className={`${className} cart-content`}>
+            <div className='cart-content__title'>
+                Cart ({count})
+            </div>
+            <div className='cart-content__products'>
 
+            </div>
+            <div className='cart-content__footer'>
+
+            </div>
         </div>
     )
 }
-
 class ShopCart extends Component {
     render() {
         const { className } = this.props;
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
-                <CartContent className='shop-cart__content'/>
+                <CartContent className='shop-cart__content' products={[243, 3434, 4554]}/>
             </div>
         )
     }
