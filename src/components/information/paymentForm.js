cc01ab51230d0f65358c71947edb7ebfb1b0de96
alexb24 <lakers76767@gmail.com>
2019-06-11 15:20:19 -0700

    added sign in functionality

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+
 import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux'; 
+
 import { FormInput, FormButton } from '../formFields';
 
 import history from '../../history';
 import OrderSummary from './orderSummary';
-
+import { UnderlinedTitle } from './infoHelp';
 class PaymentForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
-
+  
         return (
-    
             <form onSubmit={handleSubmit} className={`${className} payment-form`}>
                 <Field className='payment-form__name'
                 type='name'
@@ -18,7 +20,7 @@ class PaymentForm extends Component {
                 placeholder='Name'
                 name='name'
                 component={FormInput}/>
-                <Field className='payment-form__card'
+                 <Field className='payment-form__card'
                 type='card'
                 title='Credit Card Number'
                 placeholder='____-____-____-____'
@@ -51,6 +53,11 @@ class PaymentForm extends Component {
                 short={true}
                 component={FormButton}/>
                 <OrderSummary className='payment-form__order-summary'/>
+                <div className='payment-form__shipping-info shipping-info'>
+                    <UnderlinedTitle className='shipping-info__title' title='Shipping To'/>
+                    <div className='shipping-info__name small-text'>{this.props.name}</div>
+                    <div className='shipping-info__address small-text'>{this.props.address}</div>
+                </div>
             </form>
         )
     }
@@ -58,4 +65,12 @@ class PaymentForm extends Component {
 PaymentForm = reduxForm({
     form: 'PaymentForm'
 })(PaymentForm);
+
+function mapStateToProps(state) {
+    const { name, address } = state.user.user;
+    return { name, address }
+}
+
+PaymentForm = connect(mapStateToProps)(PaymentForm);
+
 export default PaymentForm;
